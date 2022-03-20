b4643e062ad1f27ac64b08a09f80178dd44dbc0d
Vadim Hryn <gryn.vadim@gmail.com>
2022-03-20 11:08:20 -0400

    add tests to project

---
+++ src/Event.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class Event extends Component {
+
+    state = {
+        collapsed: true
+    }
+
+    handleClick = () => {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    };
+
+  render() {
+    const { collapsed } = this.state;
+    const { event } = this.props;
+
+    return <div className="event">
+        <h2 className="summary">{event.summary}</h2>
+        <p className="start-date">
+            {event.start.dateTime} 
+            ({event.start.timeZone})
+        </p>
+        <p className="location">
+            {event.location}
+        </p>
+        <button className={`${collapsed ? "show" : "hide"}-details`} onClick={this.handleClick}>{collapsed ? "Show Details" : "Hide Details"}</button>
+    </div>;
+  }
+}
+export default Event;
---
+++ src/NumberOfEvents.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class NumberOfEvents extends Component {
+	state = {
+		numberOfEvents: '32',
+	}
+
+	handleInputChange = (event) => {
+		this.setState({
+			numberOfEvents: event.target.value,
+		});
+	}
+
+    render() {
+      return (  
+        <div className='NumberOfEvents'>
+            <input
+						type="number"
+						onChange={this.handleInputChange} 
+						value={this.state.numberOfEvents} 
+						className="numberOfEvents"/>
+        </div>
+			)	
+    }
+};    
+
+export default NumberOfEvents;
